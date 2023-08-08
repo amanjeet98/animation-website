1190cb6774dfac339e9f7a541b971bec66e1eb53
amanjeet singh <amanjeetsingh@amanjeets-MacBook-Air.local>
2023-08-08 14:29:53 +0545

    update

--- script.js
+++ script.js
@@ -34,3 +34,67 @@ gsap.to("#main",{
     }
 
 })
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=3
+        crsr.style.border="0.5px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1
+        crsr.style.border="0px solid #95c11e"
+        crsr.style.backgroundColor="#95c11e"
+    })
+} )
+
+gsap.from("#about-us img , #about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+    trigger:"#about-us",
+    scroller:"body",
+    start:"top 60%",
+    end:"top 55%",
+    scrub:3,
+    }
+})
+
+gsap.from(".cards",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+    trigger:".cards",
+    scroller:"body",
+    start:"top 70%",
+    end:"top 65%",
+    scrub:3,
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4,
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4,
+    }
+})
